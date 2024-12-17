f550f9f8a3437af08388ac54c84c32159b291105
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-12-17 17:48:09 +0100

    Add public folder files and hero image

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,9 +1,19 @@
 "use client";
 import Image from "next/image";
+import { CustomButtonProps } from "@/types";
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
-    <button disabled={false} className={`custom-btn`} onClick={() => {}}>
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {}}
+    >
       <span className={`flex-1`}>title</span>
     </button>
   );
