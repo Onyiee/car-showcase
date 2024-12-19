88fd1b49708eb1231ff717ecef2416c63e2f6679
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-12-19 22:13:57 +0100

    Add car catalogue

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,14 @@ import CustomButton from "./CustomButton";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    SearchBar,
+    CustomFilter
 }
