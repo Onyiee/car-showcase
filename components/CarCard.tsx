4db00cdcd11fd883fec3abf782c0af7fc4929737
Onyiee <elizabethonyemowo.agbo@gmail.com>
2024-12-23 23:19:08 +0100

    Fetch Corolla Cars

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
 
-const CarCard = () => {
-  return <div>CarCard</div>;
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
+
+interface CarCardProps {
+  car: CarProps;
+}
+const CarCard = ({ car }: CarCardProps) => {
+  const {
+    city_mpg,
+    class: car_class,
+    combination_mpg,
+    cylinders,
+    displacement,
+    drive,
+    fuel_type,
+    highway_mpg,
+    make,
+    model,
+    transmission,
+    year,
+  } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p>
+        <span>{carRent}</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
