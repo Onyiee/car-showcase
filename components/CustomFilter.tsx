c53c33966c432bcc3482f10aa195999fec1c419b
Onyiee <elizabethonyemowo.agbo@gmail.com>
2025-01-05 22:15:45 +0100

    Add updateSearchParams to utils

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,15 +4,25 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, ListboxButton, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
+  const router = useRouter();
+
+  const handleUpdateParams = (e: { type: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLocaleLowerCase());
+
+    router.push(newPathName);
+  };
+
   return (
     <div className="w-fit">
       <Listbox
         value={selected}
         onChange={(e) => {
           setSelected(e);
+          handleUpdateParams(e);
         }}
       >
         <div className="relative w-fit z-10">
@@ -45,7 +55,15 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                     }`
                   }
                 >
-                  {({ selected }) => <span>{option.title}</span>}
+                  {({ selected }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
